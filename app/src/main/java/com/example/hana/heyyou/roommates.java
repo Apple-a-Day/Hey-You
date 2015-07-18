c33235721b663eb55b4f50af85a972a1189a4b0d
aneedalie <t236liu@uwaterloo.ca>
2015-07-18 19:09:57 -0400

    Action bar, in progress new activity

--- app/src/main/java/com/example/hana/heyyou/roommates.java
+++ app/src/main/java/com/example/hana/heyyou/roommates.java
@@ -6,18 +6,18 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class home extends ActionBarActivity {
+public class roommates extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        setContentView(R.layout.activity_roommates);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_home, menu);
+        getMenuInflater().inflate(R.menu.menu_roommates, menu);
         return true;
     }
 
@@ -30,24 +30,9 @@ public class home extends ActionBarActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
-            return super.onOptionsItemSelected(item);
+            return true;
         }
-        switch(item.getItemId()){
-            case R.id.action_me:    //return to home
-                //openMe();
-                return true;
-            case R.id.action_rommates:
-                openRoommates();
-                return true;
-            default:
-                return true;
-        }
-    }
 
-//    public void openMe(){
-//        //open my own panel
-//    }
-    public void openRoommates(){
-        //open roommates panel
+        return super.onOptionsItemSelected(item);
     }
 }
